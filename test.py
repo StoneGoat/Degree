f1a5c5c296814740b5335e6113c0bce149c29a2a
Fellom <kali@Kali.kali>
2025-02-13 14:30:34 +0100

    Removed environment

--- test.py
+++ test.py
@@ -1,5 +1,36 @@
 import nmap
+import pydig
 
 nmapScan = nmap.PortScanner()
 
-result = nmapScan.scan("127.0.0.1", "21-443")
+ip = "192.168.0.254"
+portmin = "0"
+portmax = "5000"
+
+portrange = f"{portmin}-{portmax}"
+
+result = nmapScan.scan(ip, portrange)
+
+print(f"Nmap: {result["nmap"]}")
+print(f"Scan: {result["scan"]}")
+print(f"Tcp: {result["scan"][ip]["tcp"]}")
+
+tcp_results = result["scan"][ip]["tcp"]
+
+print("Ports open:\n")
+
+for port in tcp_results:
+    tcp_result = tcp_results[port]
+
+    str = f"Port {port} - {tcp_result["state"]}"
+
+    if (tcp_result["name"] != ""):
+        str += f", {tcp_result["name"]}"
+
+    if (tcp_result["product"] != ""):
+        str += f", {tcp_result["product"]}"
+
+    if (tcp_result["extrainfo"] != ""):
+        str += f", {tcp_result["extrainfo"]}"
+
+    print(str)
