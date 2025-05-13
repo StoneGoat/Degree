f2c67355b35ed13c03e8c3a7ef6930c5d0c4a3f9
Casper <ca223tg@student.lnu.se>
2025-05-13 12:41:19 +0200

    fixed cookie retrieval

--- session_cookie_module.py
+++ session_cookie_module.py
@@ -1,13 +1,31 @@
 import requests
-import nikto_module
+from bs4 import BeautifulSoup
 
-def get_session_cookie(login_url, username, password):
-    credentials = {
-    "username": username,
-    "password": password
-    }
+def get_dvwa_session(login_url, username, password):
     session = requests.Session()
-    resp = session.post(login_url, data=credentials)
-    resp.raise_for_status()
+
+    r1 = session.get(login_url)
+    r1.raise_for_status()
+
+    soup = BeautifulSoup(r1.text, "html.parser")
+    token = soup.find("input", {"name": "user_token"})["value"]
+
+    payload = {
+        "username":    username,
+        "password":    password,
+        "user_token":  token,
+        "Login":       "Login"
+    }
+    r2 = session.post(login_url, data=payload)
+    r2.raise_for_status()
+
+    if "Login failed" in r2.text:
+        raise Exception("DVWA login failed")
 
     return session.cookies.get_dict()
+
+print(get_dvwa_session(
+    "http://vuln.stenaeke.org/login.php",
+    "admin",
+    "password"
+))
